87e734eea0ec43ba6e9c2e2e99d44ff90584f9da
1992Arun <arunkumar.endiyur@gmail.com>
2025-02-14 23:49:48 +0530

    updated Headless

--- src/main/java/org/utility/BaseClass.java
+++ src/main/java/org/utility/BaseClass.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
@@ -27,11 +28,16 @@ public class BaseClass {
 	public static WebDriver driver;
 
 	public static void browserLaunch(String browser) {
+		
+		
+		ChromeOptions ch = new ChromeOptions();
+		
+		ch.addArguments("--headless");
 
 		switch( browser) {
 
 
-		case "Chrome": driver = new ChromeDriver();
+		case "Chrome": driver = new ChromeDriver(ch);
 
 		break;
 
